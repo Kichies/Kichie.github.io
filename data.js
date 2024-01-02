730b0138fed806307b17c5aafe04a8f9729372e4
unknown <fbhanif20@gmail.com>
2024-01-02 19:57:38 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,11 @@
+const apaAjah = "Kue Basah";
+let berapa = 2;
+
+function jualanApa(){
+    console.log(`Saya jualan ${apaAjah} sebanyak ${berapa} biji.`);
+}
+
+console.log(apaAjah);
+console.log(berapa);
+
+jualanApa();
